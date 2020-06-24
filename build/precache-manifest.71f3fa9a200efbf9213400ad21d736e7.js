4a784c65676afda26ed5ec6eb81e46e4ee0d9b2f
Stephen Lippa <you@example.com>
2020-06-24 12:11:12 -0400

    setup part two

---
+++ build/precache-manifest.71f3fa9a200efbf9213400ad21d736e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecddf4a3d582e2b9f292ec59d8f9a464",
+    "url": "/Ez-Sens/index.html"
+  },
+  {
+    "revision": "c3e5db6df3a7964e0ec1",
+    "url": "/Ez-Sens/static/css/main.f1cd2e0a.chunk.css"
+  },
+  {
+    "revision": "ec28f80ddec6594b7d9e",
+    "url": "/Ez-Sens/static/js/2.1a6dd365.chunk.js"
+  },
+  {
+    "revision": "0b14dac90b918c082faed0d9d1548202",
+    "url": "/Ez-Sens/static/js/2.1a6dd365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e5db6df3a7964e0ec1",
+    "url": "/Ez-Sens/static/js/main.a17c1370.chunk.js"
+  },
+  {
+    "revision": "43c371e91457847b1c8d",
+    "url": "/Ez-Sens/static/js/runtime-main.0379e7d3.js"
+  }
+]);
